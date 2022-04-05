8791db11ff09af666c677701d05d0ac17848c043
Rafael <up201910200@up.pt>
2022-04-05 14:59:11 +0100

    coisas

---
+++ projeto/cmake-build-debug/CMakeFiles/projeto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto.dir/main.cpp.o"
+  "projeto"
+  "projeto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projeto/cmake-build-debug/CMakeFiles/projeto.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projeto.
+# This may be replaced when dependencies are built.
